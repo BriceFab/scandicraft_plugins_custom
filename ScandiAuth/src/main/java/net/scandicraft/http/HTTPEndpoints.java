6a717adb06f23e19aa85ed4bb01e276475d2453f
BriceFab <bricefab123@gmail.com>
2020-06-07 21:06:56 +0200

    fix entry point

--- ScandiAuth/src/main/java/net/scandicraft/http/HTTPEndpoints.java
+++ ScandiAuth/src/main/java/net/scandicraft/http/HTTPEndpoints.java
@@ -2,7 +2,7 @@ package net.scandicraft.http;
 
 public class HTTPEndpoints {
 
-    public static final String BASE = "http://localhost:8000/api/";
+    public static final String BASE = "https://www.scandicraft-mc.fr/api/";
     public static final String VERIFY_TOKEN = BASE + "verify_token";
 
 }
