490634a91f08e6544c3443e5a02735cd2788cdbc
BriceFab <bricefab123@gmail.com>
2020-06-20 19:27:33 +0200

    target detect correct
    pas encore tout à fait précis

--- ScandiClasses/src/main/java/net/scandicraft/capacities/listeners/CapacitiesListener.java
+++ ScandiClasses/src/main/java/net/scandicraft/capacities/listeners/CapacitiesListener.java
@@ -6,7 +6,6 @@ import net.scandicraft.capacities.impl.GuerrierCapacity3;
 import net.scandicraft.capacities.impl.MagicienCapacity2;
 import net.scandicraft.capacities.target.ICapacityTarget;
 import net.scandicraft.capacities.target.PlayerTarget;
-import net.scandicraft.utils.MathUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
@@ -16,6 +15,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.util.Vector;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -44,7 +44,9 @@ public class CapacitiesListener implements Listener {
                 GuerrierCapacity1 g1 = new GuerrierCapacity1();
                 GuerrierCapacity3 g3 = new GuerrierCapacity3();
                 MagicienCapacity2 m2 = new MagicienCapacity2(); //TODO target PLAYER
-                ICapacityTarget target = new PlayerTarget(getTargetPlayer(player, 20));
+                int raduis = 10;
+                List<Entity> playersInRadius = player.getNearbyEntities(raduis, raduis, raduis).stream().filter(entity -> (entity instanceof Player)).collect(Collectors.toList());
+                ICapacityTarget target = new PlayerTarget((Player) getTarget(player, playersInRadius));
                 if (target.getTarget() == null) {
                     player.sendMessage(ChatColor.RED + " no target");
                 } else {
@@ -54,17 +56,39 @@ public class CapacitiesListener implements Listener {
         }
     }
 
-    public static Player getTargetPlayer(final Player fromPlayer, int raduis) {
-        List<Entity> playersInRadius = fromPlayer.getNearbyEntities(raduis, raduis, raduis).stream().filter(entity -> (entity instanceof Player)).collect(Collectors.toList());
-        for (Entity target : playersInRadius) {
-            if (target instanceof Player) {
-                if (fromPlayer.hasLineOfSight(target)) {
-                    //TODO calculer le joueur le plus proche ?
-                    return (Player) target;
-                }
+    public static <T extends org.bukkit.entity.Entity> T getTarget(final org.bukkit.entity.Entity entity, final Iterable<T> entities) {
+        if (entity == null)
+            return null;
+        T target = null;
+        final double threshold = 1;
+        for (final T other : entities) {
+            final Vector n = other.getLocation().toVector()
+                    .subtract(entity.getLocation().toVector());
+            if (entity.getLocation().getDirection().normalize().crossProduct(n)
+                    .lengthSquared() < threshold
+                    && n.normalize().dot(
+                    entity.getLocation().getDirection().normalize()) >= 0) {
+                if (target == null
+                        || target.getLocation().distanceSquared(
+                        entity.getLocation()) > other.getLocation()
+                        .distanceSquared(entity.getLocation()))
+                    target = other;
             }
         }
-        return null;
+        return target;
     }
+
+//    public static Player getTargetPlayer(final Player fromPlayer, int raduis) {
+//        List<Entity> playersInRadius = fromPlayer.getNearbyEntities(raduis, raduis, raduis).stream().filter(entity -> (entity instanceof Player)).collect(Collectors.toList());
+//        for (Entity target : playersInRadius) {
+//            if (target instanceof Player) {
+//                if (fromPlayer.hasLineOfSight(target)) {
+//                    //TODO calculer le joueur le plus proche ?
+//                    return (Player) target;
+//                }
+//            }
+//        }
+//        return null;
+//    }
 
 }
---
+++ ScandiClasses/src/main/java/net/scandicraft/utils/VectorUtils.java
@@ -0,0 +1,53 @@
+package net.scandicraft.utils;
+
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+import java.util.Collections;
+
+public class VectorUtils {
+//    public static Vector3d convert(Vector v) {
+//        return new Vector3d(v.getX(), v.getY(), v.getZ());
+//    }
+//
+//    public static Vector convert(Vector3d v) {
+//        return new Vector(v.x, v.y, v.z);
+//    }
+
+    public static Player getTargetPlayer(final Player player) {
+        return getTarget(player, player.getWorld().getPlayers());
+    }
+
+    public static boolean isLookingAt(final Player player, final Entity e) {
+        return getTarget(player, Collections.singletonList(e)) != null;
+    }
+
+    public static Entity getTargetEntity(final Entity entity) {
+        return getTarget(entity, entity.getWorld().getEntities());
+    }
+
+    public static <T extends Entity> T getTarget(final Entity entity,
+                                                 final Iterable<T> entities) {
+        if (entity == null)
+            return null;
+        T target = null;
+        final double threshold = 1;
+        for (final T other : entities) {
+            final Vector n = other.getLocation().toVector()
+                    .subtract(entity.getLocation().toVector());
+            if (entity.getLocation().getDirection().normalize().crossProduct(n)
+                    .lengthSquared() < threshold
+                    && n.normalize().dot(
+                    entity.getLocation().getDirection().normalize()) >= 0) {
+                if (target == null
+                        || target.getLocation().distanceSquared(
+                        entity.getLocation()) > other.getLocation()
+                        .distanceSquared(entity.getLocation()))
+                    target = other;
+            }
+        }
+        return target;
+    }
+}
